53d215fdf8f6b1220f6d98e73601727ac8538ede
brian <briandbutler@gmail.com>
2017-02-21 13:27:12 -0500

    setup rails app following udemy course

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+# extends ApplicationController
+class PagesController < ApplicationController
+
+  #define action (method)
+  def home
+  end
+
+  #define action (method)
+  def about
+  end
+
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+# extends ApplicationController
+class WelcomeController < ApplicationController
+
+  #define action (method)
+  def home
+  end
+
+  #define action (method)
+  def about
+  end
+
+end
